5f393e9ec51fbc97b2505c159efb0ab2e4d3b8f1
Steven Taylor <stevesay7@gmail.com>
2014-10-01 11:51:10 +0100

    extra script examples

--- JSoftware.TestFS/Script.fsx
+++ JSoftware.TestFS/Script.fsx
@@ -3,11 +3,21 @@ open FSharp.Control.JSoftware
 
 // pretty print for the c type
 fsi.AddPrinter(fun (x:c)->sprintf "((\"%s\",\"%s\",\"%s\"),(\"%s\",\"%s\"))" (x.CxInfo.Uri.ToString()) x.CxInfo.Usr x.CxInfo.Pwd (x.SerInfo.In.ToString()) (x.SerInfo.Out.ToString()))
-
 let c = new c("",65015,"sandp")
+let J = c.j
+let JD = c.jd
 
-let r = c.jd("eval 1+1")
+// paren style
+let r1 = c.jd("eval 1+1")
+let r2 = c.j("1+1")
+let r3 = c.j("reads from j")
 
-let r = c.j("1+1")
+// closure + non-paren style
+let r11 = J "1+1"
+let r12 = JD "eval 1+1"
+let r13 = JD "reads from j"
 
+// misc
+let r21 = J "i. 3 3"   // output looks like it should be inverted.  ie. 0 1 2 should be a row
 
+let r22 = J "|: i. 3 3"
